016376f08b2a401365571404ec4d4f60d3a9a29b
Luciano Nicacio <lucianonicacio.web@gmail.com>
2021-05-28 08:09:48 -0400

    single-post-cta Plugin
    Learning action and hook filters

---
+++ single-post-cta.php
@@ -0,0 +1,66 @@
+<?php
+ 
+/**
+ */
+/*
+Plugin Name: Single Post CTA
+ 
+Plugin URI: https://akismet.com/
+ 
+Description: Adds sidebar (widget area) to single posts
+ 
+Version: 0.1
+ 
+Author: Luciano Nicacio
+ 
+Author URI: https://lucianonicacio.com/
+ 
+License: GPLv2 or later
+ 
+Text Domain: spc
+ 
+*/
+
+// If this file is called directly, abort
+if (!defined('ABSPATH')) {
+    die;
+}
+
+// Load stylesheet
+function spc_load_stylesheet() {
+    if ( apply_filters('spc_load_styles', true)) {
+    if (is_single()) {
+    wp_enqueue_style('spc_stylesheet', plugin_dir_url(__FILE__) .  'spc-styles.css');
+    }
+  }
+}
+
+// Hook stylesheet
+add_action('wp_enqueue_scripts', 'spc_load_stylesheet');
+
+// Register a custom sidebar
+function spc_register_sidebar() {
+    register_sidebar( array(
+        'name' => __('Single Post CTA', 'spc'),
+        'id' => 'spc-sidebar',
+        'description' => __('Display widget area on single posts', 'spc'),
+        'before_widget' => '<div class="widget spc">',
+        'after_widget' => '</div>',
+        'before_title' => '<h2 class="widgettitle spc-title">',
+        'after_title' => '</h2>',
+    ));
+}
+
+// Hook sidebar
+add_action('widgets_init', 'spc_register_sidebar');
+
+// Display sidebar on single posts
+function spc_display_sidebar($content) {
+    if ( is_single()){
+    dynamic_sidebar('spc-sidebar');
+    return $content;
+    }
+}
+
+// Add dynamic sidebar
+add_filter( 'the_content', 'spc_display_sidebar');
